dad4ffbf1b086bb56665fa2f84578c24c6fb42ad
jjori <jjori.master@gmail.com>
2020-05-20 23:55:32 +0900

    추가. Todo입력 처리 전 form으로 부터 받은 메모 확인

--- todo_list/my_to_do_app/views.py
+++ todo_list/my_to_do_app/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'my_to_do_app/index.html')
+
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+    return HttpResponse(f'create Todo를 할거야! => {user_input_str}')
